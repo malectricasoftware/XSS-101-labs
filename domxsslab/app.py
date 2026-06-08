1b34bdf319721b59106bf1ffce8029aac0217b26
witchdocsec <107813117+witchdocsec@users.noreply.github.com>
2026-06-08 04:36:03 +0100

    Update app.py

--- domxsslab/app.py
+++ domxsslab/app.py
@@ -4,10 +4,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
                 
 @app.route("/")
-def name():
+def index():
         return render_template("index.html")
 
-@app.route("/")
+@app.route("/name")
 def name():
         return render_template("name.html")
         
